dc90ef5c9e15e4073dc269f0ac5b4df0fc6db464
dora-su <dorasu02@gmail.com>
2019-11-08 22:49:18 -0500

    coloured qr codes

--- ticket_generator.py
+++ ticket_generator.py
@@ -46,17 +46,23 @@ while True:
 
 		#colour
 		r, g, b = (0,0,0)	
+		qr_colour = '#'
 		if mod == 1 or mod == 2:
 			r, g, b = (91,150,153)
+			qr_colour = '#3F696B'
 		elif mod == 3 or mod == 4:
 			r, g, b = (198,161,139)
+			qr_colour = '#8A7061'
 		else:
 			r, g, b = (126,107,132)
+			qr_colour = '#645569'
 
 		# QR CODE GENERATION 
 		order_id = attendee['order_id']
 		checked_in = attendee['checked_in']
 		name = attendee['profile']['name'].upper()
+		if(name.lower() == 'Ucheoma ObijiNnorom'.lower()):
+			name = "Ucheoma Obiji-Nnorom".upper()
 		student_id = attendee['profile']['email'][0:9]
 
 		seq_num = "{0:0=3d}".format(i + 1)
@@ -68,15 +74,13 @@ while True:
 			border = 0,
 		)
 		table_number = str(get_column("tables.csv", student_id))
-
-		qr_data = "%s%s%s%s%s" % (name, student_id, seq_num, order_id, checked_in)
-		qr.add_data(qr_data)
+		qr_data = "%s%s%s" % (order_id, id, seq_num)qr.add_data(qr_data)
 		qr.make(fit=True)
-		qr_img = qr.make_image(fill_color='#%02x%02x%02x' % (r, g, b))
+		qr_img = qr.make_image()
 		qr_img = qr_img.resize((310,310))
-		logo = Image.open("logo.PNG")
-		logo = logo.resize((100, 100))
-		qr_img.paste(logo, (105, 105))
+		# logo = Image.open("logo.png")
+		# logo = logo.resize((100, 100))
+		# qr_img.paste(logo, (105, 105),mask=logo)
 		ticket_template.paste(qr_img, (345, 1444))
 		
 		ticket = ImageDraw.Draw(ticket_template)
@@ -85,11 +89,17 @@ while True:
 		id_font = ImageFont.truetype("Oswald-regular.ttf", 52)
 		name_font = ImageFont.truetype("Oswald-regular.ttf", 57)
 		table_font = ImageFont.truetype("Oswald-bold.ttf", 63)
+		nick_marshall_font = ImageFont.truetype("Oswald-regular.ttf",53)
 
 		#name
-		w,h = ticket.textsize(name, name_font)
-		offset = name_font.getoffset(name)
-		ticket.text(((1000-w)/2-3,66-offset[1]), name, (r,g,b), font=name_font)
+		if name = 'NICHOLAS BRIAN THOMAS MARSHALL':
+			w,h = ticket.textsize(name, nick_marshall_font)
+			offset = nick_marshall_font.getoffset(name)
+			ticket.text(((1000-w)/2-3,66-offset[1]), name, (r,g,b), font=nick_marshall_font)
+		else:
+			w,h = ticket.textsize(name, name_font)
+			offset = name_font.getoffset(name)
+			ticket.text(((1000-w)/2-3,66-offset[1]), name, (r,g,b), font=name_font)
 		# print (65+(100-h)/2)
 		#student id
 		if name == 'Janice Au Yeung' or name == 'Erik Morales' or name == 'Franklin Zhang':
@@ -102,13 +112,13 @@ while True:
 		#student id
 		w, h= ticket.textsize('TABLE ' + table_number, table_font)
 		x = (1000-w)/2+ticket.textsize('TABLE ', table_font)[0]
-		ticket.text(((1000-w)/2,1802-h+49), 'TABLE ', (0,0,0), font=table_font)
+		ticket.text(((1000-w)/2,1802-h+49), 'TABLE '+table_number, (0,0,0), font=table_font)
 		ticket.text((x,1802-h+49), table_number, (r,g,b), font=table_font)
 
-		if os.path.isfile("tickets/%s.png" % file_name):
-			ticket_template.save("tickets/%s1.png" % file_name)
+		if os.path.isfile("tickets/%s.png" % file_name.lower()):
+			ticket_template.save("tickets/%s1.png" % file_name.lower())
 		else:
-			ticket_template.save("tickets/%s.png" % file_name)
+			ticket_template.save("tickets/%s.png" % file_name.lower())
 	if(page_num == 7):
 		break
 
